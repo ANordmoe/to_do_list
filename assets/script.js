c7d5d412f921c4954617378d0692c32aacaaa39d
Alissa Nordmoe <alissa.nordmoe@gmail.com>
2021-10-18 11:22:31 -0400

    full functionality

--- assets/script.js
+++ assets/script.js
@@ -1,38 +1,39 @@
 // Show an unordered list of to do's 
 // Show an input to enter a new to do 
 // Show a button to add a to do. When the button is clicked: 
-// The text from the input box is used to add a list item to the 
-// bottom of the list 
+
 // The text from the input box is cleared out.
 
-// Extra Credit: - When a list item is clicked, cross it out, 
-// then remove it after 1 second.
+
 
 let toDoList = document.querySelector('#list');
 
 console.log(toDoList.children);
 
-function removeClicked(element) {  
-    element.remove()
+// When the user clicks on a list item, it is removed
+function removeClicked(itemClicked) {  
+    itemClicked.parentNode.removeChild(itemClicked);
 };
   
 let button = document.getElementById("addButton");
 button.onclick = function() {
     
+    // "The text from the input box is used to add a      
+    //  list item to the bottom of the list"
     let text = document.getElementById("inputId").value; 
-
     let newItem = document.createElement('li');
     newItem.innerHTML = text;
+
+    //(making them the same as the other li's)
     newItem.className = "list-group-item";
-    //newItem.onClick(removeClicked());
+    newItem.setAttribute('onclick', 'removeClicked(this)');
+
+    //adding them to the list
     document.getElementById("list").appendChild(newItem);
+
+    // The text from the input box is cleared out.
+    document.querySelector('#inputId').value = "";
 };
 
-// When the user clicks on a list item, it is removed
-        // let selectedItem = document.querySelector('.list-group-item');
-        // console.log(selectedItem, "this is the selectedItem value");
-
-
-
-
-
+// Extra Credit: - When a list item is clicked, cross it out, 
+// then remove it after 1 second.
